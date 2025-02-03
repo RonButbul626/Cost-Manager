2304097925407b6e4de65359385b06e4df3e8e5a
roychemo <roychemo@gmail.com>
2025-02-03 21:52:21 +0200

    files arrangement

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Chart from "chart.js/auto";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
